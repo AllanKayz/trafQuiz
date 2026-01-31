2c48088423e9be996864932f5059ce826c24f873
Allan Kanyemba <allankanyemba@gmail.com>
2026-01-31 11:24:43 +0200

    check  errors

--- src-electron/main.js
+++ src-electron/main.js
@@ -51,7 +51,7 @@ function createWindow() {
         });
     };
     loadApp();
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
   } else {
     const indexPath = path.join(__dirname, '../dist/trafquiz/browser/index.html');
     mainWindow.loadFile(indexPath).catch(err => {
